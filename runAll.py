00f5c9ce38116fcc1b16e23399974ced72ccbb12
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-03-04 12:16:51 +0100

    Define dictionaries for resolution plot

--- runAll.py
+++ runAll.py
@@ -8,9 +8,6 @@ print("Running with option makeTrees = {}, makePlots = {}, particleId = {}, cond
 import os, subprocess
 import  ROOT
 
-if not os.path.exists(outPath):
-    os.mkdir(outPath)
-
 treeName = "performance/tree"
 deltaBary_lim = 0.1 
 deltaAxis_lim = 0.5
@@ -18,13 +15,23 @@ histTitles = {"BaryCP": {"name": "Trackster barycenter - CaloParticle", "lim": d
 histVars = {"DEta": "#Delta#eta", "DPhi": "#Delta#phi"}
 
 for E in [10, 100]:
-  outPath = "/eos/user/l/lecriste/HGCal/www"
-  outPath = os.path.join(outPath,pid_str)
-  inputPath = outPath
-  outPath = os.path.join(outPath, str(E)+"GeV")
+  #outPath = "/eos/user/l/lecriste/HGCal/www"
+  inputPath = "/eos/user/l/lecriste/HGCal/www"
+  origin = "/afs/cern.ch/work/l/lecriste/www/HGCAL/"
+
+  inputPath = os.path.join(inputPath,pid_str)
+  outPath = os.path.join(origin,pid_str)
   if not os.path.exists(outPath):
     os.mkdir(outPath)
+    os.system('cp '+origin+'index.php '+outPath)
 
+  outPath = os.path.join(outPath, str(E)+"GeV/")
+  if not os.path.exists(outPath):
+    os.mkdir(outPath)
+    os.system('cp '+origin+'index.php '+outPath)
+
+  rms = {}
+  rmsE = {}
   for r in range(25, 160, 5):
     if makeTrees:
       bashCommand = "cmsRun run_performance_cfg.py {} {} {} {}".format(pid_str, E, nameprefix, r)
@@ -45,7 +52,15 @@ for E in [10, 100]:
 
       # TH1
       for name, title in histTitles.items():
+        if name not in rms:
+          rms[name] = {}
+          rmsE[name] = {}
+
         for varName, varLabel in histVars.items():
+          if varName not in rms[name]:
+            rms[name][varName] = {}
+            rmsE[name][varName] = {}
+
           hist = ROOT.TH1F("{}_{}".format(name,varName),"{};{}".format(title['name'],varLabel) , 100,-title['lim'],title['lim'])
           branchName = "{}_{}".format(title['branch'], "eta" if "eta" in varLabel else "phi")
           canvas = ROOT.TCanvas("canvas_"+branchName)
@@ -56,17 +71,21 @@ for E in [10, 100]:
             var = getattr(tree, branchName)
             hist.Fill(var)
 
+          rms[name][varName][format(r, '03')] = hist.GetRMS()
+          rmsE[name][varName][format(r, '03')] = hist.GetRMSError()
           hist.Draw("h")
-          canvas.Print(outPath+"/"+branchName+"_r"+str(r)+".png")
+          canvas.Print(outPath+branchName+"_r"+format(r, '03')+".png")
 
-      # TH2
-      for varName, varLabel in histVars.items():
-        hist = ROOT.TH1F("Axis-Barycenter","{varLabel};{varLabel};", 100,-title['lim'],title['lim'],100,-title['lim'],title['lim'])
-        for name, title in histTitles.items():
-          for entryNum in range(0, tree.GetEntries()):
-            tree.GetEntry(entryNum)
-            var[name] = getattr(tree, branchName)
-
-          hist.Fill(var[],var[])
+#      # TH2
+#      for varName, varLabel in histVars.items():
+#        hist = ROOT.TH1F("Axis-Barycenter","{varLabel};{varLabel};", 100,-title['lim'],title['lim'],100,-title['lim'],title['lim'])
+#        for name, title in histTitles.items():
+#          for entryNum in range(0, tree.GetEntries()):
+#            tree.GetEntry(entryNum)
+#            var[name] = getattr(tree, branchName)
+#
+#          hist.Fill(var[],var[])
 
       inFile.Close()
+
+  print(rms)
