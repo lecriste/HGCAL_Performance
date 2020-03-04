02aa1db03859a8cb96f057e19ffc4fefa2f4e5c9
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-03-04 16:28:46 +0100

    Add cosine and resolutions plots

--- runAll.py
+++ runAll.py
@@ -3,15 +3,16 @@ makeTrees = sys.argv[1].lower() == 'true'
 makePlots = sys.argv[2].lower() == 'true'
 pid_str = sys.argv[3]
 nameprefix = sys.argv[4]
+step = int(sys.argv[5])
 
-print("Running with option makeTrees = {}, makePlots = {}, particleId = {}, condition = {}".format(makeTrees, makePlots, pid_str, nameprefix))
+print("Running with option makeTrees = {}, makePlots = {}, particleId = {}, condition = {}, step = {}".format(makeTrees, makePlots, pid_str, nameprefix, step))
 import os, subprocess
 import  ROOT
 
 treeName = "performance/tree"
 deltaBary_lim = 0.1 
 deltaAxis_lim = 0.5
-histTitles = {"BaryCP": {"name": "Trackster barycenter - CaloParticle", "lim": deltaBary_lim, "branch": "EBary_cp0"}, "AxisCP": {"name": "Trackster axis - CaloParticle", "lim": deltaAxis_lim, "branch": "EAxis_cp0"}}
+histTitles = {"Bary-CP": {"name": "Trackster barycenter - CaloParticle", "lim": deltaBary_lim, "branch": "EBary_cp0", "color": ROOT.kRed}, "Axis-CP": {"name": "Trackster axis - CaloParticle", "lim": deltaAxis_lim, "branch": "EAxis_cp0", "color": ROOT.kBlack}}
 histVars = {"DEta": "#Delta#eta", "DPhi": "#Delta#phi"}
 
 for E in [10, 100]:
@@ -32,7 +33,7 @@ for E in [10, 100]:
 
   rms = {}
   rmsE = {}
-  for r in range(25, 160, 5):
+  for r in range(25, 160, step):
     if makeTrees:
       bashCommand = "cmsRun run_performance_cfg.py {} {} {} {}".format(pid_str, E, nameprefix, r)
       process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
@@ -51,6 +52,19 @@ for E in [10, 100]:
         continue
 
       # TH1
+      histCos = ROOT.TH1F("cos_BaryAxis","cos(barycenter, axis)", 100,0.9,1)
+      for entryNum in range(0, tree.GetEntries()):
+        tree.GetEntry(entryNum)
+        varCos = getattr(tree, "ts_pcaBaryEigVect0_cos")
+        histCos.Fill(varCos)
+      canvas = ROOT.TCanvas("canvas_cos_r"+format(r, '03'))
+      canvas.cd()
+      ROOT.gStyle.SetOptStat("ourme");
+      ROOT.gPad.SetLogy();
+      histCos.Draw("h")
+      canvas.Print(outPath+"cosBaryAxis_r"+format(r, '03')+".png")
+
+      ROOT.gStyle.SetOptStat("rme");
       for name, title in histTitles.items():
         if name not in rms:
           rms[name] = {}
@@ -63,7 +77,7 @@ for E in [10, 100]:
 
           hist = ROOT.TH1F("{}_{}".format(name,varName),"{};{}".format(title['name'],varLabel) , 100,-title['lim'],title['lim'])
           branchName = "{}_{}".format(title['branch'], "eta" if "eta" in varLabel else "phi")
-          canvas = ROOT.TCanvas("canvas_"+branchName)
+          canvas = ROOT.TCanvas("canvas_"+branchName+format(r, '03'))
           canvas.cd()
 
           for entryNum in range(0, tree.GetEntries()):
@@ -73,6 +87,7 @@ for E in [10, 100]:
 
           rms[name][varName][format(r, '03')] = hist.GetRMS()
           rmsE[name][varName][format(r, '03')] = hist.GetRMSError()
+          hist.SetMaximum(hist.GetEntries()/2)
           hist.Draw("h")
           canvas.Print(outPath+branchName+"_r"+format(r, '03')+".png")
 
@@ -88,4 +103,26 @@ for E in [10, 100]:
 
       inFile.Close()
 
-  print(rms)
+  from array import array
+  for varName in histVars:
+    canvas = ROOT.TCanvas("canvas_"+varName)
+    gr = {}
+    for name in histTitles:
+      n = len(rms[name][varName])
+      x, y, xE, yE = array('d'), array('d'), array('d'), array('d')
+      for point in sorted(rms[name][varName]):
+        x.append(int(point));
+        xE.append(0);
+        y.append(rms[name][varName][point])
+        yE.append(rmsE[name][varName][point])
+      gr[name] = ROOT.TGraphErrors(n, x, y, xE, yE)
+      gr[name].SetTitle( name );
+      gr[name].GetXaxis().SetTitle( "#DeltaR" )
+      gr[name].GetYaxis().SetTitle( histVars[varName] )
+      #gr[name].SetMarkerColor( histTitles[name]['color'] )
+      gr[name].SetLineColor( histTitles[name]['color'] )
+      same = 'A' if len(gr) < 2 else "SAME "
+      gr[name].Draw( same+'CP' )
+      canvas.Update()
+    ROOT.gPad.BuildLegend();
+    canvas.Print(outPath+varName+"_RMS.png")
